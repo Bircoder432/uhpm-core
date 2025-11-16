d94d0be825d38ab28a7aaf7c8114957e6885f708
Bircoder432 <yanechearer@gmail.com>
2025-11-16 22:55:43 +0500

    add package reference

--- src/models/package.rs
+++ src/models/package.rs
@@ -1,5 +1,6 @@
 use semver::Version;
 use serde::{Deserialize, Serialize};
+use std::fmt;
 use std::path::PathBuf;
 
 use crate::Dependency;
@@ -50,4 +51,60 @@ pub enum PackageSource {
 pub struct Checksum {
     pub algorithm: String,
     pub hash: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct PackageReference {
+    pub name: String,
+    pub version: Version,
+}
+
+impl PackageReference {
+    pub fn new(name: String, version: Version) -> Self {
+        Self { name, version }
+    }
+
+    pub fn from_package(package: &Package) -> Self {
+        Self {
+            name: package.name.clone(),
+            version: package.version.clone(),
+        }
+    }
+
+    pub fn id(&self) -> String {
+        format!("{}@{}", self.name, self.version)
+    }
+
+    pub fn matches(&self, other: &PackageReference) -> bool {
+        self.name == other.name && self.version == other.version
+    }
+}
+
+impl fmt::Display for PackageReference {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}@{}", self.name, self.version)
+    }
+}
+
+impl From<&Package> for PackageReference {
+    fn from(package: &Package) -> Self {
+        PackageReference::from_package(package)
+    }
+}
+
+impl TryFrom<&str> for PackageReference {
+    type Error = String;
+
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
+        let parts: Vec<&str> = s.split('@').collect();
+        if parts.len() != 2 {
+            return Err(format!("Invalid package reference format: {}", s));
+        }
+
+        let name = parts[0].to_string();
+        let version = Version::parse(parts[1])
+            .map_err(|e| format!("Invalid version in package reference: {}", e))?;
+
+        Ok(PackageReference::new(name, version))
+    }
 }
